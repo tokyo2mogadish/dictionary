278b9c3c9eb4d27285bb2723a9cf578122f12751
tokyo2mogadish <najboljiodsvih@gmail.com>
2021-09-09 19:25:59 +0200

    Zavrsena definition klasa i dodat csv fajl

--- App/definition.py
+++ App/definition.py
@@ -1,7 +1,14 @@
+import pandas
+
 class Definition:
 
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv('data.csv')
+        #print(df)
+        return tuple(df.loc[df['word'] == self.term]['definition'])
+
+defintion = Definition ('acid')
+print(defintion.get())
